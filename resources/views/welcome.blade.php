ce6abee51c8e5ff5495278dc28bc3a173bef1622
redaezziani <2003100500167@ofppt-edu.ma>
2023-10-04 17:47:32 +0200

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,29 +17,30 @@
     </head>
     <body class=" w-full  flex-col relative bg-white flex justify-start items-center">
         <nav
-        class="w-full flex border-b-[1.4px] justify-between fixed top-2 items-center px-8 py-4"
+        class="w-full bg-white flex border-b-[1.4px] justify-between fixed top-0 items-center px-8 py-4"
         
         >
             <div class="logo">
-                <h2
-                class="font-figtree text-xl font-semibold text-slate-500"
+                <img src="../../public/images/pioneerstec.png" alt="logo"
+                class="w-20"
                 >
-                    Logo 
-                </h2>
             </div>
-            <div class="cart flex justify-center items-center relative">
+            <div class="cart cursor-pointer flex justify-center items-center relative">
                 <i
                 class="fas fa-shopping-cart text-xl text-slate-900"
                 >
                 </i>
                 <div class="products-count">
                     <span
-                    class="absolute -top-2 -right-2 bg-yellow-400 text-slate-950 rounded-full px-1 text-xs"
+                    class="absolute -top-2  -right-2 bg-yellow-400 text-slate-950 rounded-full px-1 text-xs"
                     >
                         2
                     </span>
                 </div>
             </div>
         </nav>
+        <main class="rtl hero w-full flex flex-col justify-start items-center gap-3">
+
+        </main>
     </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cairo: ["Cairo", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
