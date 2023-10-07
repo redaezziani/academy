8e92e78f3f8ad6f952a47c9d5843414fa9237fcd
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-07 14:08:31 +0100

    v12

--- resources/views/courcewatche.blade.php
+++ resources/views/courcewatche.blade.php
@@ -214,40 +214,10 @@
 
         </section>
 
-    <footer
-    class="w-full z-10 flex px-16 min-h-[20rem] border-t-2 border-emerald-400 items-center justify-start gap-5 flex-col bg-slate-800 text-white py-5"
-    >
-    <div class="w-full rtl flex border-b py-2 border-slate-400 justify-between items-center ">
-        <img src="{{asset('./images/pifoneerstec.png')}}" alt="logo" class="w-10" srcset="">
-        <div class="groupe flex justify-center items-center gap-2">
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/visa.svg')}}" alt="" srcset="">
-            </div>
 
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/mastercard-2.svg')}}" alt="" srcset="">
-            </div>
+        @include('footer')
 
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/paypal.svg')}}" alt="" srcset="">
-            </div>
-        </div>
-    </div>
-    <div class="informations flex-wrap mt-2 rtl w-full  flex justify-start items-center ">
-        <div class="group-1 w-full md:w-2/3 gap-2 flex flex-col justify-start items-start">
-            <h2
-            class="text-xl text-white font-semibold"
-            >
-            تبحث عن تدريب
-            </h2>
-            <p
-            class="text-white/80 text-sm"
-            >
-            مهما كانت الدورة التدريبية التي تبحث عنها! تقدم أكاديمية المبتكر مجموعة واسعة من الدورات التدريبية والدبلومات في مجالات مختلفة، مثل الإدارة, المحاسبة, التسويق, التصميم الجرافيكي, الهندسة, البرمجة, الشبكات, واللغات. قم بتنزيل كتيبات الدورات الآن!
-            </p>
-        </div>
-    </div>
-    </footer>
+
 <!-- Add the Swiper JS -->
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
     <body class=" w-full  font-cairo overflow-x-hidden flex-col relative bg-white flex justify-start items-center">
 
         @include('navbar')
-        
+
         <main class="rtl hero  w-full flex flex-col justify-start items-center gap-3">
             <div class="bg-color z-0 absolute left-0 top-0 w-full h-full bg-slate-500/40"></div>
             <div class=" sidebar fixed shadow-md gap-5  z-50 top-0 -right-[100vw] h-screen flex flex-col min-w-[300px] p-3 justify-start items-center bg-white ">
@@ -234,40 +234,10 @@
             </div>
         </div>
     </div>
-    <footer
-    class="w-full z-10 flex px-16 min-h-[20rem] border-t-2 border-emerald-400 items-center justify-start gap-5 flex-col bg-slate-800 text-white py-5"
-    >
-    <div class="w-full rtl flex border-b py-2 border-slate-400 justify-between items-center ">
-        <img src="{{asset('./images/19d7f5fbae4ea.jpg')}}" alt="logo" class="w-14" srcset="">
-        <div class="groupe flex justify-center items-center gap-2">
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/visa.svg')}}" alt="" srcset="">
-            </div>
 
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/mastercard-2.svg')}}" alt="" srcset="">
-            </div>
+    @include('footer')
 
-            <div class="item w-14 p-2 flex bg-white rounded-md justify-center items-center" >
-            <img class="w-14" src="{{asset('./images/paypal.svg')}}" alt="" srcset="">
-            </div>
-        </div>
-    </div>
-    <div class="informations flex-wrap mt-2 rtl w-full  flex justify-start items-center ">
-        <div class="group-1 w-full md:w-2/3 gap-2 flex flex-col justify-start items-start">
-            <h2
-            class="text-xl text-white font-semibold"
-            >
-            تبحث عن تدريب
-            </h2>
-            <p
-            class="text-white/80 text-sm"
-            >
-            مهما كانت الدورة التدريبية التي تبحث عنها! تقدم أكاديمية المبتكر مجموعة واسعة من الدورات التدريبية والدبلومات في مجالات مختلفة، مثل الإدارة, المحاسبة, التسويق, التصميم الجرافيكي, الهندسة, البرمجة, الشبكات, واللغات. قم بتنزيل كتيبات الدورات الآن!
-            </p>
-        </div>
-    </div>
-    </footer>
+
 <script>
 
 let  menuAvatar=document.getElementById('menu-avatar');
