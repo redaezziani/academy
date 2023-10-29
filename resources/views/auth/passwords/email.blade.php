b70737e3bbec98ce32987eb5ab5660c12d0bd01c
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-29 10:12:16 +0100

    v12

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+@if (session('local')=='en')
+    <input type="hidden" name="" {{$dir='ltr'}}>
+    <input type="hidden" name="" {{$st='start'}}>
+@else
+<input type="hidden" name="" {{$dir='rtl'}}>
+<input type="hidden" name="" {{$st='end'}}>
+@endif
+<div dir="{{$dir}}" class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('msg.resetpass') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{ __('msg.restmsg') }}
                         </div>
                     @endif
 
@@ -18,10 +25,10 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-{{$st}}">{{ __('msg.email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" placeholder="{{ __('msg.email') }}"  style="border: 2px solid green; border-radius: 10px;"  type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -32,9 +39,9 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                            <div class="col-md-6 offset-md-4 ">
+                                <button type="submit" class="bg-success btn btn-success">
+                                    {{ __('msg.btnreset') }}
                                 </button>
                             </div>
                         </div>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -59,7 +59,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="bg-success btn btn-success">
                                     {{ __('msg.resetpass') }}
                                 </button>
                             </div>
